d9964d6539f42280cf540425bca3d9b172d12e58
Michael Zhao <michael.zhao@arm.com>
2020-02-27 12:35:42 +0000

    Helper scripts for test.
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>

---
+++ demo/download_binaries.sh
@@ -0,0 +1,3 @@
+wget https://s3.amazonaws.com/spec.ccfc.min/img/aarch64/ubuntu_with_ssh/fsfiles/xenial.rootfs.ext4 -O rootfs.img
+wget https://s3.amazonaws.com/spec.ccfc.min/img/aarch64/ubuntu_with_ssh/kernel/vmlinux.bin -O kernel.bin
+
