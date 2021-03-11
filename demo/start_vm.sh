99ccd9d6497089efc1d0899153345e2ef12c59a8
Michael Zhao <michael.zhao@arm.com>
2021-03-11 12:58:03 +0800

    improve start script

--- demo/start_vm.sh
+++ demo/start_vm.sh
@@ -1,2 +1,3 @@
-sudo ../lkvm run --disk ./rootfs.img --kernel ./kernel.bin --debug
-
+# sudo ../lkvm run --cpus 2 --disk ./rootfs.img --kernel ./kernel.bin --debug --dump-dtb test.dtb
+# sudo ../lkvm run --cpus 2 --disk ./hello-rootfs.ext4 --kernel ../../myLinux/arch/arm64/boot/Image --force-pci --debug --dump-dtb test.dtb
+sudo ../lkvm run --cpus 2 --disk ./hello-rootfs.ext4 --kernel ~/ws/src/github.com/michael2012z/myLinuxGuest/arch/arm64/boot/Image --force-pci --irqchip gicv3-its --debug --dump-dtb test.dtb
